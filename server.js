5858d40ac9b7dccd687337a9b72c6f97e79e7226
Aman Mahansaria <aman33459@gmail.com>
2020-06-15 12:57:26 +0530

    initial changes

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve our JavaScript, CSS and index.html
+app.use(express.static('./'));
+
+var pusher = new Pusher({
+  appId: '1016995',
+  key: '99114c8a4e08776d1f8a',
+  secret:  '03d0ad21e38f93a21cd3' 
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Listening at http://localhost:5000'));
